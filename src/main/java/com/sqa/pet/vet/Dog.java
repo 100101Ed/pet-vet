36221c20b9756c42f902d4df174d6a2e82f9a1f0
100101Ed <edm100101@hotmail.com>
2016-02-06 15:52:46 -0800

    Add Dog Class

---
+++ src/main/java/com/sqa/pet/vet/Dog.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: Dog.java<br>
+ * Mora, Eduardo<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.pet.vet;
+
+/**
+ * Dog //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Mora, Eduardo
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Dog extends Mammal {
+
+	private boolean performsAtShows;
+
+	private boolean wolfBreed;
+
+	private String servie;
+}
