f301a80ac5863362ee2c3cf2662b0e32f76fdedd
100101Ed <edm100101@hotmail.com>
2016-02-06 15:49:26 -0800

    Add class mammal

---
+++ src/main/java/com/sqa/pet/vet/Mammal.java
@@ -0,0 +1,26 @@
+/**
+ * File Name: Mammal.java<br>
+ * Mora, Eduardo<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.pet.vet;
+
+/**
+ * Mammal //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Mora, Eduardo
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Mammal extends Pet {
+
+	private boolean rabiesShot;
+
+	private boolean houseBroken;
+}
