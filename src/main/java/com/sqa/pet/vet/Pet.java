72d530475120567cc45f2f1fedc44f06cf175352
100101Ed <edm100101@hotmail.com>
2016-02-06 15:47:03 -0800

    Add Pet class

---
+++ src/main/java/com/sqa/pet/vet/Pet.java
@@ -0,0 +1,42 @@
+/**
+ * File Name: Pet.java<br>
+ * Mora, Eduardo<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.pet.vet;
+
+/**
+ * Pet //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Mora, Eduardo
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Pet {
+
+	private boolean alive;
+
+	private String name;
+
+	private boolean injured;
+
+	private boolean male;
+
+	private String breed;
+
+	public boolean checkPet() {
+		// TODO checkPet method implement
+		return true;
+	}
+
+	public boolean regPet() {
+		// TODO regPet method implement
+		return true;
+	}
+}
