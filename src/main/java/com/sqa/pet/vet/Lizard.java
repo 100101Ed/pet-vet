b1407517ba6977cf1b784ef8f78fda4b2b860da6
100101Ed <edm100101@hotmail.com>
2016-02-06 15:53:29 -0800

    Add Lizard Class

---
+++ src/main/java/com/sqa/pet/vet/Lizard.java
@@ -0,0 +1,26 @@
+/**
+ * File Name: Lizard.java<br>
+ * Mora, Eduardo<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.pet.vet;
+
+/**
+ * Lizard //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Mora, Eduardo
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Lizard extends Reptile {
+
+	private boolean lostTail;
+
+	private boolean changeColor;
+}
