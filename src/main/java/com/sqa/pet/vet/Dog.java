18eb0983296e195528a8bad08b222a486d440c8f
100101Ed <edm100101@hotmail.com>
2016-02-06 16:16:43 -0800

    Add Getter and Setters

--- src/main/java/com/sqa/pet/vet/Dog.java
+++ src/main/java/com/sqa/pet/vet/Dog.java
@@ -25,4 +25,49 @@ public class Dog extends Mammal {
 	private boolean wolfBreed;
 
 	private String servie;
+
+	/**
+	 * @return the servie
+	 */
+	public String getServie() {
+		return this.servie;
+	}
+
+	/**
+	 * @return the performsAtShows
+	 */
+	public boolean isPerformsAtShows() {
+		return this.performsAtShows;
+	}
+
+	/**
+	 * @return the wolfBreed
+	 */
+	public boolean isWolfBreed() {
+		return this.wolfBreed;
+	}
+
+	/**
+	 * @param performsAtShows
+	 *            the performsAtShows to set
+	 */
+	public void setPerformsAtShows(boolean performsAtShows) {
+		this.performsAtShows = performsAtShows;
+	}
+
+	/**
+	 * @param servie
+	 *            the servie to set
+	 */
+	public void setServie(String servie) {
+		this.servie = servie;
+	}
+
+	/**
+	 * @param wolfBreed
+	 *            the wolfBreed to set
+	 */
+	public void setWolfBreed(boolean wolfBreed) {
+		this.wolfBreed = wolfBreed;
+	}
 }
